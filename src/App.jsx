0397eb4274be5290cc740d019d402b56d457e2d6
Benjamin Faruna <benjaminfaruna@gmail.com>
2024-03-09 14:08:23 +0100

    Added toast for vote and delegate, refactored handleVote

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,26 @@
+import "@radix-ui/themes/styles.css";
+
 import { Box, Container, Flex, Text } from "@radix-ui/themes";
 import { configureWeb3Modal } from "./connection";
-import "@radix-ui/themes/styles.css";
+import { Toaster } from "react-hot-toast";
+
+
 import Header from "./component/Header";
 import Proposal from "./component/Proposal";
 import DelegateVote from "./component/DelegateVote";
 import useProposals from "./hooks/useProposals";
-import {
-    useWeb3ModalAccount,
-    useWeb3ModalProvider,
-} from "@web3modal/ethers/react";
-import { isSupportedChain } from "./utils";
-import { getProvider } from "./constants/providers";
-import { getProposalsContract } from "./constants/contracts";
+import useHandleVote from "./hooks/useHandleVote";
 
 configureWeb3Modal();
 
 function App() {
     const { loading, data: proposals } = useProposals();
-    const { chainId } = useWeb3ModalAccount();
-    const { walletProvider } = useWeb3ModalProvider();
 
-    const handleVote = async (id) => {
-        if (!isSupportedChain(chainId)) return console.error("Wrong network");
-        const readWriteProvider = getProvider(walletProvider);
-        const signer = await readWriteProvider.getSigner();
-
-        const contract = getProposalsContract(signer);
-
-        try {
-            const transaction = await contract.vote(id);
-            console.log("transaction: ", transaction);
-            const receipt = await transaction.wait();
-
-            console.log("receipt: ", receipt);
-
-            if (receipt.status) {
-                return console.log("vote successfull!");
-            }
-
-            console.log("vote failed!");
-        } catch (error) {
-            console.log(error);
-            let errorText;
-            if (error.reason === "Has no right to vote") {
-                errorText = "You have not right to vote";
-            } else if (error.reason === "Already voted.") {
-                errorText = "You have already voted";
-            } else {
-                errorText = "An unknown error occured";
-            }
-
-            console.error("error: ", errorText);
-        }
-    };
+    const handleVote = useHandleVote();
 
     return (
         <Container>
+            <Toaster />
             <Header />
             <main className="mt-6">
                 <Box mb="4">
